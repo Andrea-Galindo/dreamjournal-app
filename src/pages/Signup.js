5ffdbcb3daaa39cfa877e55195ab5fff4a3d83af
Andrea-Galindo <andreagalindo2103@gmail.com>
2023-08-01 17:54:15 -0400

    adds form to Signup component

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,7 +1,111 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, Button, Card, Alert, Container } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../firebase";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  // save name to store it in "users" collection. Feature currently not working
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [currentUserId, setCurrentUserId] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const { createUser } = UserAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== passwordConfirm) {
+      return setError("Passwords do not match");
+    }
+    try {
+      setError("");
+      setLoading(true);
+      await createUser(email, password).then((credentials) => {
+        setCurrentUserId(credentials.user.uid);
+        console.log(currentUserId);
+        navigate("/home");
+      });
+      await addDoc(collection(db, "users"), {
+        uid: currentUserId,
+        name: name,
+        email: email,
+      });
+      localStorage.setItem("user", name);
+
+      // console.log(email, password);
+    } catch (e) {
+      setError(e.message);
+      if (e.message === "Firebase: Error (auth/invalid-email).") {
+        setError("invalid email");
+      } else {
+      }
+      console.log(e.message);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <div>
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "50vh" }}
+      >
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Card>
+            <Card.Body>
+              {error && <Alert variant="danger">{error}</Alert>}
+              <Form onSubmit={handleSubmit}>
+                <Form.Group id="name">
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control
+                    type="name"
+                    required
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </Form.Group>
+                <Form.Group id="email">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control
+                    type="email"
+                    required
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </Form.Group>
+                <Form.Group id="password">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    required
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </Form.Group>
+                <Form.Group id="passwordConfirm">
+                  <Form.Label>Confirm Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    required
+                    onChange={(e) => setPasswordConfirm(e.target.value)}
+                  />
+                </Form.Group>
+                <Button className="w-100 custom-btn" type="submit">
+                  Sign Up
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+          <div className="w-100 text-center mt-2">
+            Already have an account? <Link to="/">Log In</Link>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default Signup;
