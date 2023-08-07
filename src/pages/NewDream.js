837eec7ea9b11e83e7e83782d0ff5168ddf0b9be
Andrea-Galindo <andreagalindo2103@gmail.com>
2023-08-07 15:52:13 -0400

    adds NewDream page

---
+++ src/pages/NewDream.js
@@ -0,0 +1,12 @@
+import React from "react";
+import DreamForm from "../components/DreamForm";
+
+const NewDream = () => {
+  return (
+    <div className="new-dream-page">
+      <DreamForm />
+    </div>
+  );
+};
+
+export default NewDream;
