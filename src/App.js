d990a1a2f5344b469e4a0068066cba445156e219
Andrea-Galindo <andreagalindo2103@gmail.com>
2023-08-07 09:59:32 -0400

    adds ProtectedRoute component so only authenticated users can navigate through the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
+import NewDream from "./pages/NewDream";
 
 function App() {
   return (
@@ -12,7 +14,22 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />}></Route>
           <Route path="/signup" element={<Signup />}></Route>
-          <Route path="/home" element={<Home />}></Route>
+          <Route
+            path="/home"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          ></Route>
+          <Route
+            path="/newdream"
+            element={
+              <ProtectedRoute>
+                <NewDream />
+              </ProtectedRoute>
+            }
+          ></Route>
         </Routes>
       </AuthContextProvider>
     </div>
