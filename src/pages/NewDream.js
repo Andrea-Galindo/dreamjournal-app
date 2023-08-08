19ced373138d126fca3b31459b290c517e713f13
Andrea-Galindo <andreagalindo2103@gmail.com>
2023-08-08 16:13:51 -0400

    adds AppNavBar component to NewDream page and style file

--- src/pages/NewDream.js
+++ src/pages/NewDream.js
@@ -1,9 +1,13 @@
 import React from "react";
 import DreamForm from "../components/DreamForm";
+import AppNavBar from "../components/AppNavBar";
+
+import "../styles/NewDream.css";
 
 const NewDream = () => {
   return (
     <div className="new-dream-page">
+      <AppNavBar />
       <DreamForm />
     </div>
   );
