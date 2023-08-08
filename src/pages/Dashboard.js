ab8d39723b591f67d6e363a2f86ecaf7392a60ea
Andrea-Galindo <andreagalindo2103@gmail.com>
2023-08-08 16:08:35 -0400

    adds AppNavBar component to Dashboard page and changes styles

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { LinkContainer } from "react-router-bootstrap";
-import { Container, Button } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { PiPlusCircleThin } from "react-icons/pi";
 
 import { db } from "../firebase";
 import {
@@ -14,6 +15,8 @@ import {
 } from "firebase/firestore";
 
 import Dream from "../components/Dream";
+import AppNavBar from "../components/AppNavBar";
+import "../styles/Dashboard.css";
 
 const Dashboard = () => {
   const navigate = useNavigate();
@@ -104,42 +107,48 @@ const Dashboard = () => {
 
   return (
     <div className="dreams-page">
-      {/* <AppNavbar /> */}
+      <AppNavBar />
       <Container className="d-flex justify-content-center mt-3">
-        <div className="w-100 m-2" style={{ maxWidth: "600px" }}>
+        <div className="w-100 m-2" style={{ maxWidth: "390px" }}>
           <input
+            style={{
+              // borderRadius: "40px",
+              fontFamily: "degular-text, sans-serif",
+              border: "none",
+            }}
             className="form-control form-search-input"
-            placeholder="Search by people and places"
+            placeholder="Search person or place"
             type="text"
             onChange={(event) => handleSearch(event)}
           />
         </div>
       </Container>
-      {dreams ? (
-        <div style={{ padding: 10 }}>
-          {dreams.map((dream) => {
-            return (
-              <Dream
-                key={dream.id}
-                id={dream.id}
-                date={dream.date}
-                title={dream.title}
-                description={dream.description}
-                feelings={dream.feelings}
-                peopleandplaces={dream.peopleandplaces}
-                onEdit={handleEdit}
-                onDelete={handleDelete}
-              />
-            );
-          })}
-        </div>
-      ) : (
-        <div className="m-5 text-center no-records">
-          You have not yet created dream records
-        </div>
-      )}
-      <LinkContainer to="/newdream">
-        <Button className="new-dream-btn">create</Button>
+      <div style={{ padding: 10 }}>
+        {filteredDreams.map((dream) => {
+          return (
+            <Dream
+              key={dream.id}
+              id={dream.id}
+              date={dream.date}
+              title={dream.title}
+              description={dream.description}
+              feelings={dream.feelings}
+              peopleandplaces={dream.peopleandplaces}
+              onEdit={handleEdit}
+              onDelete={handleDelete}
+            />
+          );
+        })}
+      </div>
+      {/* <div className="m-5 text-center no-records">
+        You have not yet created dream records
+      </div> */}
+
+      <LinkContainer
+        style={{ fontSize: "3rem", color: "rgb(129, 94, 234" }}
+        to="/newdream"
+      >
+        <PiPlusCircleThin />
       </LinkContainer>
     </div>
   );
